cf7c07d96af3fcabe428cc70f240dc5958c48ed8
Pedro <pedropacheco92@outlook.com>
2017-05-18 20:30:41 -0300

    navegador

---
+++ medcenter.app/src/main/java/com/medcenter/medcenter/app/navigator/Navigator.java
@@ -0,0 +1,24 @@
+package com.medcenter.medcenter.app.navigator;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewDisplay;
+import com.vaadin.server.Page.PopStateEvent;
+import com.vaadin.server.Page.PopStateListener;
+
+public class Navigator implements ViewDisplay, PopStateListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void uriChanged(PopStateEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showView(View view) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
