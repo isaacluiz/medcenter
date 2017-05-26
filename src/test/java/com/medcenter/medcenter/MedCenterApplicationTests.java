b0c2f57c66dcbeb92a31d1a12602249ab9aabda0
pedro <pedropacheco@92@outlook.com>
2017-05-26 10:05:30 -0300

    dando boa 2

--- src/test/java/com/medcenter/medcenter/MedCenterApplicationTests.java
+++ src/test/java/com/medcenter/medcenter/MedCenterApplicationTests.java
@@ -1,4 +1,4 @@
-package com.medcenter.medcenter.app;
+package com.medcenter.medcenter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class MedCenterApplicationTests {
 
 	@Test
 	public void contextLoads() {
